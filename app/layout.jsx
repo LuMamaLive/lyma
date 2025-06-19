8c3ef010091f1f44b756d406f5dfb0d48577170e
Genius <mom4us007@gmail.com>
2025-06-19 06:36:41 -0400

    will it work?

--- app/layout.jsx
+++ app/layout.jsx
@@ -3,7 +3,7 @@ export const metadata = {
   description: 'Your pregnancy journey starts here.',
 };
 
-export default function RootLayout({ children }: { children: React.ReactNode }) {
+export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>{children}</body>
